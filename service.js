ceb0948880e0a62859012e6484d60026025c325f
Eugene Yagrushkin <eyagrushkin@ritchiebros.com>
2019-05-22 15:28:48 -0700

    add css file and update some styles

--- service.js
+++ service.js
@@ -1,27 +1,35 @@
-function init() {
+function init(nextDay) {
   
     var site_0 = document.getElementById("wframe_0");
     //woodside
-    site_0.src = getURL(0);
+    site_0.src = getURL(0, nextDay);
     
      var site_0 = document.getElementById("wframe_1");
     //bridal
-    site_0.src = getURL(1);
+    site_0.src = getURL(1, nextDay);
   
      var site_0 = document.getElementById("wframe_2");
     //benny
-    site_0.src = getURL(5);
+    site_0.src = getURL(5, nextDay);
   
     var site_0 = document.getElementById("wframe_3");
     //pemby
-    site_0.src = getURL(5);
+    site_0.src = getURL(15, nextDay);
     
   }
   
-  function getURL(zone){
-    var thisYear = new Date().getFullYear();
-    var thisMonth = new Date().getMonth() + 1;
-    var thisDay = new Date().getDate();
+  function getURL(zone, nextDay){
+    
+    // if(nextDay = nil){
+    //   nextDay = 0;
+    // }
+    
+    var ms = new Date().getTime() + 86400000 * nextDay;
+    var date = new Date(ms);
+    
+    var thisYear = date.getFullYear();
+    var thisMonth = date.getMonth() + 1;
+    var thisDay = date.getDate();
     
     if(thisMonth < 10){
       thisMonth = "0" + thisMonth;
