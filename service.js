d08efdb65531f6baf353ec5f34b0ca8d41fd908a
Eugene Yagrushkin <yagrushkin@gmail.com>
2020-06-22 17:55:32 -0700

    Update service.js

--- service.js
+++ service.js
@@ -4,25 +4,37 @@ function init(nextDay) {
     //woodside
     site_0.src = getURL(0, nextDay);
     
-     var site_0 = document.getElementById("wframe_1");
+    var site_0 = document.getElementById("wframe_1");
     //bridal
     site_0.src = getURL(1, nextDay);
   
-     var site_0 = document.getElementById("wframe_2");
+    var site_0 = document.getElementById("wframe_2");
     //benny
     site_0.src = getURL(5, nextDay);
-  
+
     var site_0 = document.getElementById("wframe_3");
+    //Little Nic
+    site_0.src = getURL(131, nextDay);
+
+    var site_0 = document.getElementById("wframe_4");
     //pemby
     site_0.src = getURL(15, nextDay);
-    
+
+    var site_0 = document.getElementById("wframe_6");
+    //Oscar, Savona
+    site_0.src = getURL(47, nextDay);
+
+    var site_0 = document.getElementById("wframe_7");
+    //Hedly
+    site_0.src = getURL(40, nextDay);
+
+    var site_0 = document.getElementById("wframe_8");
+    //Lumby
+    site_0.src = getURL(29, nextDay);
+
   }
   
   function getURL(zone, nextDay){
-    
-    // if(nextDay = nil){
-    //   nextDay = 0;
-    // }
     
     var ms = new Date().getTime() + 86400000 * nextDay;
     var date = new Date(ms);
